761393d686359384f179c8027c4bbaac2e90a267
João Mello <79203254+jbfmello@users.noreply.github.com>
2026-03-18 13:42:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,9 @@ name_on_smoothie = st.text_input(
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(F.col("SEARCH_ON"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 ingredient_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
